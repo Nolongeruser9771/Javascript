3b101a755357d4334d0ddf9c356d6b95e798c030
nguyenntt <nt.thaonguyen25@gmail.com>
2023-03-08 16:54:36 +0700

    lesson01_exercise

---
+++ lesson01.js
@@ -0,0 +1,65 @@
+//Bài 1
+let n1;
+do {
+    n1 = parseFloat(prompt("Nhập 1 số nguyên dương"));
+} while(!Number.isInteger(n1) || n1<0);
+
+function calculateFactorial(n) {
+    let fatorial = 1;
+    for(let i=1; i<=n;i++) {
+        fatorial *=i;
+    }
+    return fatorial;
+}
+console.log("Giai thừa của " + n1 + " là: " + calculateFactorial(n1));
+
+//Bài 2
+let string = prompt("NhậP 1 chuỗi bất kì");
+
+function reverseString(string) {
+    let reverseString ="";
+    for(let i = string.length-1; i>=0; i--) {
+        reverseString+=string.charAt(i);
+    }
+    return reverseString;
+}
+console.log("Chuỗi đảo ngược là: " + reverseString(string));
+
+//Bài 3
+let countryCode = prompt("Nhập mã quốc gia");
+
+function translate(code) {
+    let greeting;
+    switch(code.toLowerCase()) {
+        
+        case "VN".toLowerCase():
+            greeting ="Xin chào";
+            break;
+        case "EN".toLowerCase():
+            greeting ="Hello";
+            break;  
+        case "JP".toLowerCase():
+            greeting ="Konichiwa";
+            break;  
+        case "FR".toLowerCase():
+            greeting ="Bonjour";
+            break;
+        default:
+            greeting = "Undefined translate!"
+    }
+    return greeting;
+}
+
+console.log(translate(countryCode));
+
+//Bài 4:
+let s;
+do {
+    s = prompt("Nhập chuỗi dài hơn 15 kí tự");
+} while(s.length<15);
+
+function subString(s) {
+    return s.substring(0,10) +"...";
+}
+
+console.log("Chuỗi rút gon: " + subString(s));
