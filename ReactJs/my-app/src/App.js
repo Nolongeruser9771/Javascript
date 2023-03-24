2dd724576a2780bdbbc83df57b4f0c1958930205
nguyenntt <106979668+Nolongeruser9771@users.noreply.github.com>
2023-03-24 18:50:07 +0700

    Render Color box

--- ReactJs/my-app/src/App.js
+++ ReactJs/my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ItemRender from './components/ItemRender';
+import RenderBox from './components/RenderBox';
 
 function App() {
   return (
     <div className='App'>
       <ItemRender />
+      <RenderBox />
     </div>
   );
 }
