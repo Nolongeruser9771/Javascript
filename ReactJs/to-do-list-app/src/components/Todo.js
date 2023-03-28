a4e5fd5f1a7f6d01f3b605711d967382ad00b663
nguyenntt <106979668+Nolongeruser9771@users.noreply.github.com>
2023-03-29 03:30:31 +0700

    todolist

---
+++ ReactJs/to-do-list-app/src/components/Todo.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+function Todo({ todo, onDeleteBtnClick, onCheckBtnClick, onEditBtnClick}) {
+    //tô đỏ task done
+    const [textColor, setTextColor] = useState("white");
+    const [textDecor, setTextDecor] = useState("none");
+
+    useEffect(() => {
+        if(todo.isDone==="y") return (
+            setTextColor("firebrick"),
+            setTextDecor("line-through")
+        )
+        else return (
+            setTextColor("white"),
+            setTextDecor("none")
+        )
+    },[todo])
+
+    const attributes = {
+        color: textColor,
+        textDecoration: textDecor
+    }
+    return (
+        <div className="task-container">
+            <li>
+                <input id="checkBox" type="checkbox" onClick={()=>onCheckBtnClick(todo.id)}></input>
+                <span style={attributes}>
+                    {todo.name}
+                </span>
+            </li>
+            <div className="task-btn-container">
+                <button className="modifiedBtn" onClick={()=>onEditBtnClick(todo.id)}>Edit</button>
+                <button className="modifiedBtn" onClick={()=>onDeleteBtnClick(todo.id)}>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
